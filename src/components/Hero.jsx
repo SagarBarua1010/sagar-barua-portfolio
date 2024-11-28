0d067160045cffd4e610009f4c8000e3786a9e5a
SagarBarua1010 <sagarbarua1010@gmail.com>
2024-11-28 19:39:55 +0600

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HeroImage from "../assets/hero-image.png";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-
+const resumeUrl =
+  "https://drive.google.com/file/d/1lSd-PaULdd8YAXcNsaYRsG8DC7Lg3ebm/view?usp=sharing";
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-r from-slate-900 to-slate-700 text-white text-center py-16">
+    <div className="bg-gradient-to-r  from-slate-900 to-slate-700 text-white text-center py-16">
       <img
         src={HeroImage}
-        alt=""
+        alt="sagar barua"
         className="mx-auto mb-8 w-48 h-48 rounded-full object-cover object-left-top transform 
         transition-transform duration-300 hover:scale-105 bg-gradient-to-r from-pink-600 to-purple-500"
       />
@@ -24,31 +25,33 @@ const Hero = () => {
       <div className="flex space-x-4 my-4 md:my-0 max-w-64 mx-auto items-center justify-center">
         <a
           href="https://www.linkedin.com/in/sagarbarua0/"
-          className="text-gray-400 hover:text-white"
+          className="text-gray-400 hover:text-white "
         >
           <FaLinkedin />
         </a>
         <a
           href="https://github.com/SagarBarua1010"
-          className="text-gray-400 hover:text-white"
+          className="text-gray-400 hover:text-white "
         >
           <FaGithub />
         </a>
       </div>
       <div className="mt-4 space-x-4">
         <a
+          href={resumeUrl}
+          target="_blank"
+          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+        >
+          View Resume
+        </a>
+        <a
           href="#contact-me"
           className="bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
-          Contact With Me
+          Hire Me
         </a>
-        <button
-          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
-        >
-          Resume
-        </button>
       </div>
     </div>
   );
